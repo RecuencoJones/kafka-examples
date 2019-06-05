4c2ad12de45f12166391b50a39e314ee09eb4dc5
David Recuenco <david.recuenco@adidas-group.com>
2019-06-05 14:23:48 +0200

    ADD consumer-dotnet

---
+++ consumer-dotnet/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+using DotNetEnv;
+using Confluent.Kafka;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        DotNetEnv.Env.Load();
+
+        var conf = new ConsumerConfig
+        {
+            BootstrapServers = System.Environment.GetEnvironmentVariable("KAFKA_HOST"),
+            GroupId = System.Environment.GetEnvironmentVariable("KAFKA_GROUP_ID"),
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
+
+        using (var consumer = new ConsumerBuilder<Ignore, string>(conf).Build())
+        {
+            consumer.Subscribe(System.Environment.GetEnvironmentVariable("KAFKA_TOPIC"));
+
+            Console.WriteLine("Consumer started, press <CTRL> + C to exit");
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, e) => {
+                e.Cancel = true;
+
+                cts.Cancel();
+            };
+
+            try
+            {
+                while (true)
+                {
+                    var message = consumer.Consume(cts.Token);
+                    Console.WriteLine(message.Value);
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                consumer.Close();
+            }
+        }
+    }
+}
